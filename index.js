443ca7a47634a7941bd6d387cd276401fb32e8ab
khalelalbuquerque <khaleledipo@gmail.com>
2023-05-03 20:09:00 -0300

    Home HMTL

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express()
 // MAKEUP http://makeup-api.herokuapp.com/api/v1/products.json
 
 app.engine('handlebars', exphbs.engine())
-app.set('handlebars', 'handlebars')
+app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 app.use(express.json())
@@ -18,13 +18,15 @@ app.get('/', async (req,res)=>{
 
     await axios.get('http://makeup-api.herokuapp.com/api/v1/products.json')
     .then(data=>{
-        console.log(data.data.length)
+        
+        const randomizedItems = data.data.sort(()=>Math.random()-0.5)
+        const items = randomizedItems.splice(0,15)
+
+        res.render('products/home', {items})
     })
+    .catch(err=>console.log(err))
 
 })
-
-
-
 
 
 app.listen(3000)
